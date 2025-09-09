69141558d0915a74743426de7b1fd4709561124a
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-09-10 02:49:17 +0530

    configure base url

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,15 +1,1 @@
-// api.ts
-import axios from "axios";
-
-export const BASE_URL =
-  location.hostname === "localhost"
-    ? "http://localhost:7777" // your local server
-    : "https://devmatch-backend.vercel.app"; // your serverless backend
-
-export const api = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true, // cookies (if you set auth cookies)
-});
-
-// usage
-// api.post("/login", body)
+export const BASE_URL=location.hostname==="localhost" ? "http://localhost:7777" :"/api"
